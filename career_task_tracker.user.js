6542152be4b1d5a6adc0365e82d8ae9bf8ed554c
Moritz Lenz <moritz.lenz@gmail.com>
2019-07-13 14:12:01 +0200

    show link to /career
    ... when data is missing for the current station

--- career_task_tracker.user.js
+++ career_task_tracker.user.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name         career_task_tracker
-// @version      1.1
+// @version      1.2
 // @author       Moritz Lenz <moritz.lenz@gmail.com>
 // @description  Submit current career task rewards to a central collector. Please get an access token from moritz and add it in your preferences page.
-// @match        https://alpha.taustation.space/career
-// @match        https://alpha.taustation.space/career/*
-// @match        https://alpha.taustation.space/preferences
+// @match        https://alpha.taustation.space/
+// @match        https://alpha.taustation.space/*
 // @require      https://code.jquery.com/jquery-3.3.1.min.js
 // @require      https://rawgit.com/taustation-fan/userscripts/master/userscript-preferences.js
 // @grant        none
@@ -13,9 +12,14 @@
 
 (function() {
     'use strict';
+    let base_url = 'https://ctt.tauguide.de/v1/';
     function status_message(message) {
         $('p#ctt_msg').remove();
         $('div.career-task-container').after('<p id="ctt_msg">Career task tracker: ' + message + '</p>')
+    }
+
+    function get_station() {
+        return $('span.station').text().trim();
     }
 
     function pref_specs() {
@@ -33,62 +37,74 @@
         };
     }
 
-    let options = userscript_preferences( pref_specs() );
+    var station = get_station();
+    if (station && window.location.pathname.match('^/career')) {
+        let options = userscript_preferences( pref_specs() );
 
-    if ( !window.location.pathname.match('^/career') ) {
-        return;
-    }
-    var token = options.token;
-    if (!token) {
-        status_message('Please configure your access token in the user preferences');
-        return;
-    }
+        if ( !window.location.pathname.match('^/career') ) {
+            return;
+        }
+        var token = options.token;
+        if (!token) {
+            status_message('Please configure your access token in the user preferences');
+            return;
+        }
 
-    var station = $('span.station').text().trim();
-    var career_chunks = $('div#employment_panel').find('li:Contains("Career")').find('a').text().trim().split(' - ');
-    var career = career_chunks[0];
-    var rank = career_chunks[1];
+        var career_chunks = $('div#employment_panel').find('li:Contains("Career")').find('a').text().trim().split(' - ');
+        var career = career_chunks[0];
+        var rank = career_chunks[1];
 
-    var tasks = {};
-    $('.table-career-tasks').each(function(table_idx) {
-        $(this).find('tr').each(function() {
-            var $row = $( this );
-            var name = $row.find('td').eq(0).text();
-            if (!name) return;
-            var amount = $row.find('.currency-amount').text();
-            tasks[name] = amount;
+        var tasks = {};
+        $('.table-career-tasks').each(function(table_idx) {
+            $(this).find('tr').each(function() {
+                var $row = $( this );
+                var name = $row.find('td').eq(0).text();
+                if (!name) return;
+                var amount = $row.find('.currency-amount').text();
+                tasks[name] = amount;
+            });
         });
-    });
-    if ($.isEmptyObject(tasks)) {
-        status_message('No career tasks found');
-        return;
+        if ($.isEmptyObject(tasks)) {
+            status_message('No career tasks found');
+            return;
+        }
+
+        var payload = {
+            token: token,
+            station: station,
+            career: career,
+            rank: rank,
+            tasks: tasks,
+        };
+
+        let url = base_url + 'add';
+
+        $.ajax({
+            type: "POST",
+            url: url,
+            dataType: 'json',
+            data: JSON.stringify(payload),
+            success: function(response) {
+                if (response.recorded) {
+                    status_message('tasks successfully recorded. +1 brownie point!');
+                }
+                else {
+                    status_message('error recording tasks: ' + response.message);
+                }
+            },
+            error: function(xhr) {
+                status_message('cannot talk to ' + url + ': ' + xhr.response_text);
+            },
+        });
     }
-
-    var payload = {
-        token: token,
-        station: station,
-        career: career,
-        rank: rank,
-        tasks: tasks,
-    };
-
-    let url = 'https://ctt.tauguide.de/v1/add';
-
-    $.ajax({
-        type: "POST",
-        url: url,
-        dataType: 'json',
-        data: JSON.stringify(payload),
-        success: function(response) {
-            if (response.recorded) {
-                status_message('tasks successfully recorded. +1 brownie point!');
+    else if (station) {
+        let url = base_url + 'station-needs-update/' + encodeURIComponent(station);
+        console.log('getting ' + url);
+        $.get(url, function (response) {
+            console.log('Response: ', response);
+            if (respones.needs_update) {
+                $('span.employment-title:contains(Career)').parent().append('– <a href="/career">Check tasks</a>');
             }
-            else {
-                status_message('error recording tasks: ' + response.message);
-            }
-        },
-        error: function(xhr) {
-            status_message('cannot talk to ' + url + ': ' + xhr.response_text);
-        },
-    });
+        });
+    }
 }());
