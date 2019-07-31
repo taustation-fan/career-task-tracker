99ddf38e469f9dde3c8bb5fe459fae34172f7417
Moritz Lenz <moritz.lenz@gmail.com>
2019-07-31 18:09:11 +0200

    Switch to postgres

--- ctt/__init__.py
+++ ctt/__init__.py
@@ -9,7 +9,8 @@ from ctt.model import db, Character, Token, CareerTask, BatchSubmission, TaskRea
 
 def make_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.environ.get('CTT_DB', '/tmp/test.db')
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.environ.get('CTT_DB', '/tmp/test.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///ctt'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     CORS(app)
